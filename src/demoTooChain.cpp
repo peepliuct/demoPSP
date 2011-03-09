d8555d2297680a5f1d5022a93ad6b0a055cca72e
roy.liu <Roy.Liu@bj-royliu.iaSolution.net>
2011-03-09 15:19:53 +0800

    create a simple project.

---
+++ src/demoTooChain.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : demoTooChain.cpp
+// Author      : roy
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	return 0;
+}
